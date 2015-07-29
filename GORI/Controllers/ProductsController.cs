048dd4e6e5fbada55040f849719093ea7ac64d5f
curiousnadir <nadir@innostark.com>
2015-07-29 19:04:38 +0500

    GORI Products

--- GORI/Controllers/ProductsController.cs
+++ GORI/Controllers/ProductsController.cs
@@ -1,101 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace GORI.Controllers
 {
     public class ProductsController : Controller
     {
-        // GET: Products
-        public ActionResult Index()
+        // GET: Products/Gori
+        public ActionResult GORI22()
         {
             return View();
         }
-
-        // GET: Products/Details/5
-        public ActionResult Details(int id)
-        {
-            if (id == 22)
-            {
-                return View("GORI22");
-            }
-            return View();
-        }
-        // GET: Products/Gori/5
-        public ActionResult GORI(int id)
-        {
-            if (id == 22)
-            {
-                return View("GORI22");
-            }
-            return View();
-        }
-        // GET: Products/Create
-        public ActionResult Create()
+        public ActionResult GORI44()
         {
             return View();
         }
-
-        // POST: Products/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Products/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult GORI44Plus()
         {
             return View();
         }
-
-        // POST: Products/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Products/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult GORI79()
         {
             return View();
         }
-
-        // POST: Products/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult GORI88()
         {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            return View();
+        }
+        public ActionResult GORI91()
+        {
+            return View();
         }
     }
 }
